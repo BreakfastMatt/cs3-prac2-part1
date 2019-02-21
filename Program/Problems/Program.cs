285befc9d8618009992f92f85a22fd767a233006
Matthew Lewis <G17L3581@ict.ru.ac.za>
2019-02-21 15:08:30 +0200

    Added the updated solution. csproj and cs files as well as the Problems.Test cs and csproj files

--- Program/Problems/Program.cs
+++ Program/Problems/Program.cs
@@ -2,11 +2,38 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(Prefix("test"));
+        }
+
+        static string Prefix(string input)
+        {
+            //Special case: input is null, return null.
+            if(input == null) 
+			{
+                return null;
+			}
+            //Setup
+            int len = input.Length;//is this even a thing idk -- check.
+            int numWords = 0;
+            bool sawSpace = false;
+            //Body
+            for(int i=0;i<len;i++){
+                if(input[i] == ' ') { sawSpace = true; }
+                if(input[i] != ' ' && sawSpace == true)
+                {
+                    sawSpace = false;
+                    numWords++;
+                }
+				if(numWords == 0 && input[i] != ' ') { numWords = 1; } //So the first word, isn't missed
+            }
+            //Output
+            string output = $"{len},{numWords}:{input}";
+            return output;
         }
     }
 }
