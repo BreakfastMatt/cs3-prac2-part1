4ef3205d8865c408d45e7639e9f6fa79c9e4240e
matthew lewis <16mdlewis@gmail.com>
2019-02-22 18:41:05 +0200

    Added the raw log files as well as changed Problems.Tests to use a single Test method

--- Program/Problems.Tests/UnitTest1.cs
+++ Program/Problems.Tests/UnitTest1.cs
@@ -12,41 +12,54 @@ namespace Tests
             
         }
 
+
         [Test]
 		//test case i
         public void Test1()
         {
+            //Test 1:
             //Arrange
             Program p = new Program();
             //Act
             string i = p.Prefix("hello");
             //Assert
             Assert.AreEqual("5,1:hello",i);
-        }
-
-		[Test]
-		//test case ii
-		public void Test2()
-		{
-            //Arrange
-            Program p = new Program();
+            
+            //Test 2:
+            //Arrange -- none since all test cases use p
             //Act
             string ii = p.Prefix("");
             //Assert
             Assert.AreEqual("0,0:",ii);
-		}
-		
-		[Test]
-		//test case iii
-		public void Test3()
-		{
-            //Arrange
-            Program p = new Program();
+
+            //Test 3:
+            //Arrange -- none since all test cases use p 
             //Act
             string iii = p.Prefix("what ... did you say?? ");
             //Assert
-			//Prac says it's 27, but it should be 23...
-            Assert.AreEqual("23,5:what ... did you say?? ",iii);
-		}
+            //practical says that length is 27, but is actually 23.
+             Assert.AreEqual("23,5:what ... did you say?? ",iii);
+
+             //Test 4:  (extra case 1)
+             //Arrange -- again not needed
+             //Act
+             string iv = p.Prefix("  how many  words  is this  even??   ");
+             //Assert
+             Assert.AreEqual("37,6:  how many  words  is this  even??   ",iv);
+
+             //Test 5: (extra case 2)
+             //Arrange -- not needed
+             //Act
+             string v = p.Prefix(" lots of spaces   in these words  indeed      ... yeah ");
+             //Assert
+             Assert.AreEqual("55,9: lots of spaces   in these words  indeed      ... yeah ",v);
+
+             //Test 6: (extra case 3)
+             //Arrange -- note needed
+             //Act 
+             string vi = p.Prefix("          ");
+             //Assert
+             Assert.AreEqual("10,0:          ",vi);
+        }
     }
 }
